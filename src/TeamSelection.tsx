f083b5ab82a1d7330ca6c5014d85d0ba6b13acd1
Joseph Daniel Winters <jdwinters96@gmail.com>
2024-01-15 18:18:56 -0500

    Clean up clicking logic

--- src/TeamSelection.tsx
+++ src/TeamSelection.tsx
@@ -4,51 +4,52 @@ import UsaMap from "./assets/usa-map.svg";
 import { teams } from "./teams";
 
 function TeamSelection() {
-  const [team1, setTeam1] = useState<string | null>(null);
-  const [team2, setTeam2] = useState<string | null>(null);
+  const [homeTeam, setHomeTeam] = useState<string | null>(null);
+  const [awayTeam, setAwayTeam] = useState<string | null>(null);
 
   return (
     <div className="team-select-wrapper">
       <h2 className="team-select-heading">Select a team</h2>
       <div id="map-wrapper">
         <img alt="Map of the United States" id="usa-map" src={UsaMap} />
-        {teams.map((team) => (
-          <div
-            className="team-wrapper"
-            style={{ left: `${team.coords.x}%`, top: `${team.coords.y}%` }}
-          >
+        {teams.map((team) => {
+          const { accent, bg, identifier } = team.colours;
+          const isHomeTeam = homeTeam === team.id;
+          const isAwayTeam = awayTeam === team.id;
+
+          return (
             <div
-              role="button"
-              className="team-marker"
-              style={{
-                backgroundColor:
-                  team1 === team.id || team2 === team.id
-                    ? `var(--${team.colours.bg})`
-                    : "",
-                borderColor:
-                  team1 === team.id || team2 === team.id
-                    ? `var(--${team.colours.accent})`
-                    : `var(--${team.colours.identifier})`,
-              }}
-              onClick={() => {
-                if (team1 === team.id) {
-                  setTeam1(null);
-                } else if (team2 === team.id) {
-                  setTeam2(null);
-                } else if (
-                  team1 === null ||
-                  (team1 === null) === (team2 === null)
-                ) {
-                  setTeam1(team.id);
-                } else {
-                  setTeam2(team.id);
-                }
-              }}
+              className="team-wrapper"
+              style={{ left: `${team.coords.x}%`, top: `${team.coords.y}%` }}
             >
-              {team.icon}
+              <div
+                role="button"
+                className="team-marker"
+                style={{
+                  backgroundColor:
+                    isHomeTeam || isAwayTeam ? `var(--${bg})` : "",
+                  borderColor:
+                    isHomeTeam || isAwayTeam
+                      ? `var(--${accent})`
+                      : `var(--${identifier})`,
+                }}
+                onClick={() => {
+                  if (isHomeTeam) {
+                    setHomeTeam(null);
+                  } else if (isAwayTeam) {
+                    setAwayTeam(null);
+                  } else if (homeTeam === null) {
+                    setHomeTeam(team.id);
+                  } else if (awayTeam === null) {
+                    setAwayTeam(team.id);
+                  }
+                }}
+              >
+                {team.icon}
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );

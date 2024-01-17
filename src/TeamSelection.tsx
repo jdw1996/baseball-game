1668aa331aa0b06fe59ad86d77e671bc23942675
Joseph Daniel Winters <jdwinters96@gmail.com>
2024-01-17 18:39:41 -0500

    Bug fix plus add match-up summary

--- src/TeamSelection.tsx
+++ src/TeamSelection.tsx
@@ -1,37 +1,96 @@
 import { useState } from "react";
 import "./TeamSelection.css";
 import UsaMap from "./assets/usa-map.svg";
-import { teams } from "./teams";
+import { teamsMap } from "./teams";
+
+const UnknownTeam = () => (
+  <div
+    className="team-marker-inline"
+    style={{
+      borderColor: "var(--grey)",
+    }}
+  >
+    ❓
+  </div>
+);
 
 function TeamSelection() {
   const [homeTeam, setHomeTeam] = useState<string | null>(null);
   const [awayTeam, setAwayTeam] = useState<string | null>(null);
 
+  console.log(Object.entries(teamsMap));
   return (
     <div className="team-select-wrapper">
-      <h2 className="team-select-heading">Select a team</h2>
+      <h2 className="team-select-heading">Select your teams</h2>
+      <div className="team-select-summary">
+        <span>Today's match-up:</span>
+        {awayTeam ? (
+          <span
+            className="team-marker-inline"
+            style={{
+              backgroundColor: `var(--${teamsMap.get(awayTeam)?.colours.bg})`,
+              borderColor: `var(--${teamsMap.get(awayTeam)?.colours.accent})`,
+            }}
+          >
+            {teamsMap.get(awayTeam)?.icon}
+          </span>
+        ) : (
+          <UnknownTeam />
+        )}
+        <span>@</span>
+        {homeTeam ? (
+          <span
+            className="team-marker-inline"
+            style={{
+              backgroundColor: `var(--${teamsMap.get(homeTeam)?.colours.bg})`,
+              borderColor: `var(--${teamsMap.get(homeTeam)?.colours.accent})`,
+            }}
+          >
+            {teamsMap.get(homeTeam)?.icon}
+          </span>
+        ) : (
+          <UnknownTeam />
+        )}
+        <button
+          onClick={() => {
+            const oldHomeTeam = homeTeam;
+            setHomeTeam(awayTeam);
+            setAwayTeam(oldHomeTeam);
+          }}
+        >
+          Swap home and away
+        </button>
+        <button
+          onClick={() => {
+            alert("TODO: Not implemented!");
+          }}
+        >
+          Play ball!
+        </button>
+      </div>
       <div id="map-wrapper">
         <img alt="Map of the United States" id="usa-map" src={UsaMap} />
-        {teams.map((team) => {
-          const { accent, bg, identifier } = team.colours;
-          const isHomeTeam = homeTeam === team.id;
-          const isAwayTeam = awayTeam === team.id;
+        {Array.from(teamsMap.entries()).map(([id, team]) => {
+          console.log({ id, team });
+          const { colours, coords } = team;
+          const isHomeTeam = homeTeam === id;
+          const isAwayTeam = awayTeam === id;
 
           return (
             <div
               className="team-wrapper"
-              style={{ left: `${team.coords.x}%`, top: `${team.coords.y}%` }}
+              style={{ left: `${coords.x}%`, top: `${coords.y}%` }}
             >
               <div
                 role="button"
                 className="team-marker"
                 style={{
                   backgroundColor:
-                    isHomeTeam || isAwayTeam ? `var(--${bg})` : "",
+                    isHomeTeam || isAwayTeam ? `var(--${colours.bg})` : "",
                   borderColor:
                     isHomeTeam || isAwayTeam
-                      ? `var(--${accent})`
-                      : `var(--${identifier})`,
+                      ? `var(--${colours.accent})`
+                      : `var(--${colours.identifier})`,
                 }}
                 onClick={() => {
                   if (isHomeTeam) {
@@ -39,9 +98,9 @@ function TeamSelection() {
                   } else if (isAwayTeam) {
                     setAwayTeam(null);
                   } else if (homeTeam === null) {
-                    setHomeTeam(team.id);
+                    setHomeTeam(id);
                   } else if (awayTeam === null) {
-                    setAwayTeam(team.id);
+                    setAwayTeam(id);
                   }
                 }}
               >
--- src/teams.ts
+++ src/teams.ts
@@ -29,371 +29,431 @@ export type Team = {
     y: number;
   };
   icon: string;
-  id: string;
 };
 
-export const teams: Team[] = [
-  {
-    city: "Anaheim",
-    colours: {
-      accent: "navy",
-      bg: "red",
-      identifier: "red",
-      text: "yellow",
+export const teamsMap: Map<string, Team> = new Map([
+  [
+    "LAA",
+    {
+      city: "Anaheim",
+      colours: {
+        accent: "navy",
+        bg: "red",
+        identifier: "red",
+        text: "yellow",
+      },
+      coords: { x: 13.1, y: 59.1 },
+      icon: "👼",
+      id: "LAA",
     },
-    coords: { x: 13.1, y: 59.1 },
-    icon: "👼",
-    id: "LAA",
-  },
-  {
-    city: "Arizona",
-    colours: {
-      accent: "teal",
-      bg: "brick",
-      identifier: "brick",
-      text: "white",
+  ],
+  [
+    "ARI",
+    {
+      city: "Arizona",
+      colours: {
+        accent: "teal",
+        bg: "brick",
+        identifier: "brick",
+        text: "white",
+      },
+      coords: { x: 21.3, y: 64 },
+      icon: "🐍",
     },
-    coords: { x: 21.3, y: 64 },
-    icon: "🐍",
-    id: "ARI",
-  },
-  {
-    city: "Atlanta",
-    colours: {
-      accent: "navy",
-      bg: "red",
-      identifier: "navy",
-      text: "white",
+  ],
+  [
+    "ATL",
+    {
+      city: "Atlanta",
+      colours: {
+        accent: "navy",
+        bg: "red",
+        identifier: "navy",
+        text: "white",
+      },
+      coords: { x: 71, y: 65.3 },
+      icon: "🔨",
     },
-    coords: { x: 71, y: 65.3 },
-    icon: "🔨",
-    id: "ATL",
-  },
-  {
-    city: "Baltimore",
-    colours: {
-      accent: "black",
-      bg: "orange",
-      identifier: "black",
-      text: "orange",
+  ],
+  [
+    "BAL",
+    {
+      city: "Baltimore",
+      colours: {
+        accent: "black",
+        bg: "orange",
+        identifier: "black",
+        text: "orange",
+      },
+      coords: { x: 82.3, y: 42 },
+      icon: "🐧",
     },
-    coords: { x: 82.3, y: 42 },
-    icon: "🐧",
-    id: "BAL",
-  },
-  {
-    city: "Boston",
-    colours: {
-      accent: "navy",
-      bg: "red",
-      identifier: "red",
-      text: "red",
+  ],
+  [
+    "BOS",
+    {
+      city: "Boston",
+      colours: {
+        accent: "navy",
+        bg: "red",
+        identifier: "red",
+        text: "red",
+      },
+      coords: { x: 89.3, y: 27.8 },
+      icon: "🅱️",
     },
-    coords: { x: 89.3, y: 27.8 },
-    icon: "🅱",
-    id: "BOS",
-  },
-  {
-    city: "Chicago",
-    colours: {
-      accent: "red",
-      bg: "royal",
-      identifier: "royal",
-      text: "white",
+  ],
+  [
+    "CHC",
+    {
+      city: "Chicago",
+      colours: {
+        accent: "red",
+        bg: "royal",
+        identifier: "royal",
+        text: "white",
+      },
+      coords: { x: 63.2, y: 36.1 },
+      icon: "🧸",
     },
-    coords: { x: 63.2, y: 36.1 },
-    icon: "🧸",
-    id: "CHC",
-  },
-  {
-    city: "Chicago",
-    colours: {
-      accent: "grey",
-      bg: "black",
-      identifier: "grey",
-      text: "white",
+  ],
+  [
+    "CWS",
+    {
+      city: "Chicago",
+      colours: {
+        accent: "grey",
+        bg: "black",
+        identifier: "grey",
+        text: "white",
+      },
+      coords: { x: 63.2, y: 40.6 },
+      icon: "🧦",
     },
-    coords: { x: 63.2, y: 40.6 },
-    icon: "🧦",
-    id: "CWS",
-  },
-  {
-    city: "Cincinnati",
-    colours: {
-      accent: "red",
-      bg: "black",
-      identifier: "red",
-      text: "white",
+  ],
+  [
+    "CIN",
+    {
+      city: "Cincinnati",
+      colours: {
+        accent: "red",
+        bg: "black",
+        identifier: "red",
+        text: "white",
+      },
+      coords: { x: 69.3, y: 45.6 },
+      icon: "🔴",
     },
-    coords: { x: 69.3, y: 45.6 },
-    icon: "🔴",
-    id: "CIN",
-  },
-  {
-    city: "Cleveland",
-    colours: {
-      accent: "navy",
-      bg: "red",
-      identifier: "navy",
-      text: "navy",
+  ],
+  [
+    "CLE",
+    {
+      city: "Cleveland",
+      colours: {
+        accent: "navy",
+        bg: "red",
+        identifier: "navy",
+        text: "navy",
+      },
+      coords: { x: 73.3, y: 36.9 },
+      icon: "🕷",
     },
-    coords: { x: 73.3, y: 36.9 },
-    icon: "🕷",
-    id: "CLE",
-  },
-  {
-    city: "Colorado",
-    colours: {
-      accent: "grey",
-      bg: "purple",
-      identifier: "purple",
-      text: "white",
+  ],
+  [
+    "COL",
+    {
+      city: "Colorado",
+      colours: {
+        accent: "grey",
+        bg: "purple",
+        identifier: "purple",
+        text: "white",
+      },
+      coords: { x: 35.1, y: 43.8 },
+      icon: "🏔",
     },
-    coords: { x: 35.1, y: 43.8 },
-    icon: "🏔",
-    id: "COL",
-  },
-  {
-    city: "Detroit",
-    colours: {
-      accent: "orange",
-      bg: "navy",
-      identifier: "navy",
-      text: "white",
+  ],
+  [
+    "DET",
+    {
+      city: "Detroit",
+      colours: {
+        accent: "orange",
+        bg: "navy",
+        identifier: "navy",
+        text: "white",
+      },
+      coords: { x: 70.6, y: 34.3 },
+      icon: "🐅",
     },
-    coords: { x: 70.6, y: 34.3 },
-    icon: "🐅",
-    id: "DET",
-  },
-  {
-    city: "Houston",
-    colours: {
-      accent: "navy",
-      bg: "orange",
-      identifier: "orange",
-      text: "white",
+  ],
+  [
+    "HOU",
+    {
+      city: "Houston",
+      colours: {
+        accent: "navy",
+        bg: "orange",
+        identifier: "orange",
+        text: "white",
+      },
+      coords: { x: 51.1, y: 81 },
+      icon: "🌠",
     },
-    coords: { x: 51.1, y: 81 },
-    icon: "🌠",
-    id: "HOU",
-  },
-  {
-    city: "Kansas City",
-    colours: {
-      accent: "gold",
-      bg: "royal",
-      identifier: "royal",
-      text: "white",
+  ],
+  [
+    "KC",
+    {
+      city: "Kansas City",
+      colours: {
+        accent: "gold",
+        bg: "royal",
+        identifier: "royal",
+        text: "white",
+      },
+      coords: { x: 52.4, y: 48.7 },
+      icon: "👑",
     },
-    coords: { x: 52.4, y: 48.7 },
-    icon: "👑",
-    id: "KC",
-  },
-  {
-    city: "Los Angeles",
-    colours: {
-      accent: "grey",
-      bg: "royal",
-      identifier: "royal",
-      text: "white",
+  ],
+  [
+    "LAD",
+    {
+      city: "Los Angeles",
+      colours: {
+        accent: "grey",
+        bg: "royal",
+        identifier: "royal",
+        text: "white",
+      },
+      coords: { x: 10.2, y: 58 },
+      icon: "🚎",
     },
-    coords: { x: 10.2, y: 58 },
-    icon: "🚎",
-    id: "LAD",
-  },
-  {
-    city: "Miami",
-    colours: {
-      accent: "red",
-      bg: "black",
-      identifier: "black",
-      text: "sky",
+  ],
+  [
+    "MIA",
+    {
+      city: "Miami",
+      colours: {
+        accent: "red",
+        bg: "black",
+        identifier: "black",
+        text: "sky",
+      },
+      coords: { x: 81, y: 91 },
+      icon: "🐟",
     },
-    coords: { x: 81, y: 91 },
-    icon: "🐟",
-    id: "MIA",
-  },
-  {
-    city: "Milwaukee",
-    colours: {
-      accent: "yellow",
-      bg: "navy",
-      identifier: "yellow",
-      text: "yellow",
+  ],
+  [
+    "MIL",
+    {
+      city: "Milwaukee",
+      colours: {
+        accent: "yellow",
+        bg: "navy",
+        identifier: "yellow",
+        text: "yellow",
+      },
+      coords: { x: 62.7, y: 31.6 },
+      icon: "🍺",
     },
-    coords: { x: 62.7, y: 31.6 },
-    icon: "🍺",
-    id: "MIL",
-  },
-  {
-    city: "Minnesota",
-    colours: {
-      accent: "red",
-      bg: "navy",
-      identifier: "navy",
-      text: "white",
+  ],
+  [
+    "MIN",
+    {
+      city: "Minnesota",
+      colours: {
+        accent: "red",
+        bg: "navy",
+        identifier: "navy",
+        text: "white",
+      },
+      coords: { x: 54.3, y: 27.5 },
+      icon: "♊",
     },
-    coords: { x: 54.3, y: 27.5 },
-    icon: "♊",
-    id: "MIN",
-  },
-  {
-    city: "New York",
-    colours: {
-      accent: "navy",
-      bg: "grey",
-      identifier: "grey",
-      text: "navy",
+  ],
+  [
+    "NYY",
+    {
+      city: "New York",
+      colours: {
+        accent: "navy",
+        bg: "grey",
+        identifier: "grey",
+        text: "navy",
+      },
+      coords: { x: 84.3, y: 32.7 },
+      icon: "🎩",
     },
-    coords: { x: 84.3, y: 32.7 },
-    icon: "🎩",
-    id: "NYY",
-  },
-  {
-    city: "New York",
-    colours: {
-      accent: "orange",
-      bg: "royal",
-      identifier: "orange",
-      text: "orange",
+  ],
+  [
+    "NYM",
+    {
+      city: "New York",
+      colours: {
+        accent: "orange",
+        bg: "royal",
+        identifier: "orange",
+        text: "orange",
+      },
+      coords: { x: 86.7, y: 34.75 },
+      icon: "🍎",
     },
-    coords: { x: 86.7, y: 34.75 },
-    icon: "🍎",
-    id: "NYM",
-  },
-  {
-    city: "Oakland",
-    colours: {
-      accent: "yellow",
-      bg: "green",
-      identifier: "green",
-      text: "white",
+  ],
+  [
+    "OAK",
+    {
+      city: "Oakland",
+      colours: {
+        accent: "yellow",
+        bg: "green",
+        identifier: "green",
+        text: "white",
+      },
+      coords: { x: 7, y: 42.7 },
+      icon: "🐘",
     },
-    coords: { x: 7, y: 42.7 },
-    icon: "🐘",
-    id: "OAK",
-  },
-  {
-    city: "Philadelphia",
-    colours: {
-      accent: "sky",
-      bg: "brick",
-      identifier: "brick",
-      text: "white",
+  ],
+  [
+    "PHI",
+    {
+      city: "Philadelphia",
+      colours: {
+        accent: "sky",
+        bg: "brick",
+        identifier: "brick",
+        text: "white",
+      },
+      coords: { x: 84.3, y: 38.3 },
+      icon: "🔔",
     },
-    coords: { x: 84.3, y: 38.3 },
-    icon: "🔔",
-    id: "PHI",
-  },
-  {
-    city: "Pittsburgh",
-    colours: {
-      accent: "black",
-      bg: "yellow",
-      identifier: "yellow",
-      text: "black",
+  ],
+  [
+    "PIT",
+    {
+      city: "Pittsburgh",
+      colours: {
+        accent: "black",
+        bg: "yellow",
+        identifier: "yellow",
+        text: "black",
+      },
+      coords: { x: 76, y: 39.7 },
+      icon: "🏴‍☠️",
     },
-    coords: { x: 76, y: 39.7 },
-    icon: "🏴‍☠️",
-    id: "PIT",
-  },
-  {
-    city: "San Diego",
-    colours: {
-      accent: "yellow",
-      bg: "brown",
-      identifier: "brown",
-      text: "white",
+  ],
+  [
+    "SD",
+    {
+      city: "San Diego",
+      colours: {
+        accent: "yellow",
+        bg: "brown",
+        identifier: "brown",
+        text: "white",
+      },
+      coords: { x: 11.9, y: 63.1 },
+      icon: "⛪",
     },
-    coords: { x: 11.9, y: 63.1 },
-    icon: "⛪",
-    id: "SD",
-  },
-  {
-    city: "San Francisco",
-    colours: {
-      accent: "black",
-      bg: "orange",
-      identifier: "orange",
-      text: "black",
+  ],
+  [
+    "SF",
+    {
+      city: "San Francisco",
+      colours: {
+        accent: "black",
+        bg: "orange",
+        identifier: "orange",
+        text: "black",
+      },
+      coords: { x: 4.3, y: 42.7 },
+      icon: "🌉",
     },
-    coords: { x: 4.3, y: 42.7 },
-    icon: "🌉",
-    id: "SF",
-  },
-  {
-    city: "Seattle",
-    colours: {
-      accent: "navy",
-      bg: "teal",
-      identifier: "teal",
-      text: "white",
+  ],
+  [
+    "SEA",
+    {
+      city: "Seattle",
+      colours: {
+        accent: "navy",
+        bg: "teal",
+        identifier: "teal",
+        text: "white",
+      },
+      coords: {
+        x: 11.5,
+        y: 9,
+      },
+      icon: "🧭",
     },
-    coords: {
-      x: 11.5,
-      y: 9,
+  ],
+  [
+    "STL",
+    {
+      city: "St. Louis",
+      colours: {
+        accent: "red",
+        bg: "navy",
+        identifier: "red",
+        text: "white",
+      },
+      coords: { x: 59.3, y: 49.2 },
+      icon: "🐦",
     },
-    icon: "🧭",
-    id: "SEA",
-  },
-  {
-    city: "St. Louis",
-    colours: {
-      accent: "red",
-      bg: "navy",
-      identifier: "red",
-      text: "white",
+  ],
+  [
+    "TB",
+    {
+      city: "Tampa Bay",
+      colours: {
+        accent: "navy",
+        bg: "sky",
+        identifier: "sky",
+        text: "navy",
+      },
+      coords: { x: 75.4, y: 84.4 },
+      icon: "☀️",
     },
-    coords: { x: 59.3, y: 49.2 },
-    icon: "🐦",
-    id: "STL",
-  },
-  {
-    city: "Tampa Bay",
-    colours: {
-      accent: "navy",
-      bg: "sky",
-      identifier: "sky",
-      text: "navy",
+  ],
+  [
+    "TEX",
+    {
+      city: "Texas",
+      colours: {
+        accent: "red",
+        bg: "royal",
+        identifier: "royal",
+        text: "white",
+      },
+      coords: { x: 48.3, y: 68.9 },
+      icon: "🤠",
     },
-    coords: { x: 75.4, y: 84.4 },
-    icon: "☀️",
-    id: "TB",
-  },
-  {
-    city: "Texas",
-    colours: {
-      accent: "red",
-      bg: "royal",
-      identifier: "royal",
-      text: "white",
+  ],
+  [
+    "TOR",
+    {
+      city: "Toronto",
+      colours: {
+        accent: "navy",
+        bg: "royal",
+        identifier: "royal",
+        text: "white",
+      },
+      coords: { x: 76, y: 28 },
+      icon: "🍁",
     },
-    coords: { x: 48.3, y: 68.9 },
-    icon: "🤠",
-    id: "TEX",
-  },
-  {
-    city: "Toronto",
-    colours: {
-      accent: "navy",
-      bg: "royal",
-      identifier: "royal",
-      text: "white",
+  ],
+  [
+    "WSH",
+    {
+      city: "Washington",
+      colours: {
+        accent: "pink",
+        bg: "grey",
+        identifier: "pink",
+        text: "white",
+      },
+      coords: { x: 80.6, y: 45.4 },
+      icon: "🇺🇸",
     },
-    coords: { x: 76, y: 28 },
-    icon: "🍁",
-    id: "TOR",
-  },
-  {
-    city: "Washington",
-    colours: {
-      accent: "pink",
-      bg: "grey",
-      identifier: "pink",
-      text: "white",
-    },
-    coords: { x: 80.6, y: 45.4 },
-    icon: "🇺🇸",
-    id: "WSH",
-  },
-];
+  ],
+]);
